faf25a32265722113ae95b5be47ec9ff1bf44749
Marden145 <moragafallas@gmail.com>
2025-08-10 23:07:06 -0600

    update

---
+++ AnalisisWeb/Web/Pages/Categorias/Categorias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Categorias
+{
+    public class CategoriasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
