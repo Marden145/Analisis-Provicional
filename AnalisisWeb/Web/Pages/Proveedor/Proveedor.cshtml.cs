ec16a574027e7c600e8dc5c817963d89276c1e39
Marden145 <moragafallas@gmail.com>
2025-08-12 09:32:57 -0600

    parte final

---
+++ AnalisisWeb/Web/Pages/Proveedor/Proveedor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Proveedor
+{
+    public class ProveedorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
